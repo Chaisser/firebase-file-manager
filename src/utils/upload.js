57e23304e94c05cfbac06be47a22fbde163a18f4
Doruk Karaboncuk <doruk.karaboncuk@gmail.com>
2019-11-14 07:35:55 +0300

    First commit

--- src/utils/upload.js
+++ src/utils/upload.js
@@ -1,9 +1,7 @@
 import { storage } from "./../firebase/firebase";
 
 const uploadToStorage = async file => {
-  console.log(file);
   const fileName = `${new Date().getTime()}-${file.name}.jpg`;
-  console.log(fileName);
   const upload = new Promise((resolve, reject) => {
     if (!file) {
       return reject({
@@ -47,30 +45,3 @@ const uploadToStorage = async file => {
 };
 
 export default uploadToStorage;
-
-// const uploadTask = storage
-//   .ref("/")
-//   .child(`${new Date().getTime()}-${file.name}.jpg`);
-
-// uploadTask.put(file).on(
-//   "state_changed",
-//   function(snapshot) {
-//     var getProgress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-//     setProgress(getProgress);
-//   },
-//   function(error) {
-//     alert("hata", error);
-//   },
-//   function() {
-//     const updatedFiles = files.filter(
-//       deletefile => file.name != deletefile.name
-//     );
-//     console.log("bitti", updatedFiles);
-//     console.log("files", files);
-//     database.ref("/doruk").push({
-//       photo: `${new Date().getTime()}.jpg`
-//     });
-
-//     return setFiles(updatedFiles);
-//   }
-// );
